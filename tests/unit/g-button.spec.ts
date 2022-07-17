796267656391090bcc98847527bf2ad283e89324
omGazz89 <omgazz89@gmail.com>
2022-07-17 16:15:25 +0200

    added text props

--- tests/unit/g-button.spec.ts
+++ tests/unit/g-button.spec.ts
@@ -17,4 +17,14 @@ describe('GButton.vue', () => {
     expect(wrapper.props().text).toBe(text);
     expect(wrapper.props().text).not.toBe(randomText);
   })
+
+  it("have the text prop to be a string", () => {
+    const text = "foo";
+
+    const wrapper = shallowMount(GButton, {
+      props: { text }
+    })
+
+    expect(typeof wrapper.props().text).toBe("string")
+  })
 })
